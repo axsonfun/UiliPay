6fda9930a90cc573d08e1e909b2034e1f39813ac
不朽千秋 <541420514@qq.com>
2019-01-06 19:57:48 +0800

    0.1版本发布

---
+++ js/paul.js
@@ -0,0 +1,10 @@
+function loader() {
+    var loader = document.getElementById("loader");
+    setTimeout(function () {
+        loader.classList.add("hidden");
+    }, 2000);
+}
+loader();
+if (window.console && window.console.log) {
+    console.log("\n %c Lo-Li.CN %c https://uili.cn  By:不朽千秋","color: #fff; background: #1979ca; padding:5px 0;","background: #efefef; padding:5px 0;");
+}
